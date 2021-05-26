040f1c2664d5224db019005daa94423ea1c6a193
braynaci24 <aynacimuhammetemin@gmail.com>
2021-05-26 16:16:02 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+$(document).ready(function () {
+
+  let favorites = JSON.parse(localStorage.getItem('favorites')) || []
+
+  $('.form-button').click(function () {
+    $('.form-container').animate({
+      left: '300px',
+      top: '200px',
+      height: 'toggle'
+    }, 2000)
+  })
+
+  $('.favorites-button').click(function () {
+    $('.favorites').show();
+  })
+
+  $('.favorites-hearth-icon').click(function(){
+    let imagesAttr = $(this).parent().prev().attr('src');
+    favorites.push(imagesAttr)
+    localStorage.setItem('favorites', favorites);
+
+    $('.favorites').append(`<img  class="n" src='${imagesAttr}'>`);
+    
+  })
+
+})
+
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
   var navbarHeight = $(".navbar").height();
