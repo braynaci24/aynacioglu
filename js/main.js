a9cb8b49454116b2c0babec25eea5acad37b305e
braynaci24 <aynacimuhammetemin@gmail.com>
2021-05-28 14:59:42 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
   let data = JSON.parse(localStorage.getItem('favorites')) || []
 
+  for(let i = 0; i < data.length; i++){
+    $('.favorites').append(`<div class="favorites-card"> <img class="fav-images" src="${data[i].images}"> <span class="">${data[i].name}</span><a class="trash" href="#"><i class="far fa-trash-alt"></i></a></div>`)
+  }
+
   $('.form-button').click(function () {
     $('.form-container').animate({
       left: '300px',
@@ -18,19 +22,24 @@ $(document).ready(function () {
     let imagesAttr = $(this).parent().prev().attr('src');
     let pieceName = $(this).prev().text();
     let pieceObject = {
-      "images": imagesAttr,
-      "name": pieceName,
+      images: imagesAttr,
+      name: pieceName,
     }
     data.push(pieceObject);
     localStorage.setItem('favorites', JSON.stringify(data));
+    $('.favorites').append(`<div class="favorites-card"> <img class="fav-images" src="${pieceObject.images}"> <span>${pieceObject.name}</span><a class="trash" href="#"><i class="far fa-trash-alt"></i></a></div>`)
    
   })
-/* 
+
   $('body').on('click', '.trash', function () {
-    let srcTrash = $(this).prev().prev().attr('src');
-    let textDelete = $(this).prev().text();
+    let _text = $(this).prev().text();
+    let ind = data.indexOf(_text);
+    data.splice(ind,1)
+    localStorage.setItem('favorites', JSON.stringify(data));
+    $(this).parent().remove();
+   
   })
- */
+
 
   $('.favorites-close-icon').click(function () {
     $('.favorites').hide();
