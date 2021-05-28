12205384ffffb305aaa45fa110a799e3e1ce32a9
braynaci24 <aynacimuhammetemin@gmail.com>
2021-05-28 11:49:53 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
 $(document).ready(function () {
   let data = JSON.parse(localStorage.getItem('favorites')) || []
-  let dataPieceName = JSON.parse(localStorage.getItem('piece')) || []
-  for (let i = 0; i < data.length; i++) {
-    $('.favorites').append(`<div class="favorites-card"><img class="fav-images" src=" ${data[i]}"><i class="far fa-trash-alt trash"></i></div>`)
-    for(let j = 0; j < dataPieceName.length; i++){
-      $('.favorites').append(`<div class="favorites-card"><span class="b">${dataPieceName[i]}</span><i class="far fa-trash-alt trash"></i></div>`)
 
-    }
-  }
 
   $('.form-button').click(function () {
     $('.form-container').animate({
@@ -24,23 +17,19 @@ $(document).ready(function () {
   $('.favorites-hearth-icon').click(function () {
     let imagesAttr = $(this).parent().prev().attr('src');
     let pieceName = $(this).prev().text();
-    dataPieceName.push(pieceName)
-    data.push(imagesAttr);
-    localStorage.setItem('piece', JSON.stringify(dataPieceName))
-    localStorage.setItem('favorites', JSON.stringify(data))
-    $('.favorites').append(`<div class="favorites-card"><img class="fav-images" src=" ${imagesAttr}"> <span>${pieceName}</span><i class="far fa-trash-alt trash"></i></div>`)
+    let pieceObject = {
+      name: pieceName,
+      images: imagesAttr,
+    }
+    
+    data.push(pieceObject);
+    localStorage.setItem('favorites', JSON.stringify(pieceObject));
+    $('.favorites').append(`<div class="favorites-card"><img class="fav-images" src=" ${pieceObject.images}"> <span>${pieceObject.name}</span><i class="far fa-trash-alt trash"></i></div>`)
   })
 
   $('body').on('click', '.trash', function () {
     let srcTrash = $(this).prev().prev().attr('src');
     let textDelete = $(this).prev().text();
-    let indxText = dataPieceName.indexOf(textDelete)
-    let indx = data.indexOf(srcTrash);
-    data.splice(indx,1 )
-    dataPieceName.splice(indxText, 1)
-    localStorage.setItem('favorites', JSON.stringify(data));
-    localStorage.setItem('piece', JSON.stringify(dataPieceName));
-    $(this).parent().remove();
   })
 
 
